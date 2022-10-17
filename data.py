3921b9afa6e7e7cfb4fff8f956bb1f68bc0b1394
Gennadiy <renardes@gmail.com>
2022-10-17 23:07:11 +0500

    общий раздел - 4. работа с данными в приложении

--- data.py
+++ data.py
@@ -5,6 +5,15 @@
 STATS = {}
 SAVES = {}
 
+PLAYERS = ['PlayerA', 'PlayerB']
+TOKENS = ('X', 'O')
+
+DIM = 3
+RANGE = range(DIM)
+RANGE_FLAT = range(DIM**2)
+
+TURNS = []
+BOARD = [''] * DIM**2
 
 # глобальные константы
 APP_TITLE = "КРЕСТИКИ-НОЛИКИ"
